b90da062b17e9aad28900ab00661059075d8bc44
Shannon McCartney <smccar117@cable.comcast.com>
2021-05-16 19:47:05 -0400

    added save button and click event with local storage

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ $(document).ready(function () {
   var todayDate = moment().format("dddd, MMM Do YYYY");
   $("#currentDay").html(todayDate);
 });
+
+// Save button & click event
+
+$(document).ready(function () {
+  var text = $(this).sibiling(".description").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, text);
+});
+
+function trackHours() {
+  var currentTime = moment().hour();
+}
