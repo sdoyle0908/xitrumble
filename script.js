364170c4e6043ada4f95f8d4624c0e2e75dd4f7e
Shannon McCartney <smccar117@cable.comcast.com>
2021-05-16 20:07:40 -0400

    added if else for checking time and adding classes

--- script.js
+++ script.js
@@ -3,23 +3,41 @@ $(document).ready(function () {
 
   var todayDate = moment().format("dddd, MMM Do YYYY");
   $("#currentDay").html(todayDate);
-});
 
-// Save button & click event
+  // Save button & click event
 
-$(document).ready(function () {
-  var text = $(this).sibiling(".description").val();
-  var time = $(this).parent().attr("id");
+  $(document).ready(function () {
+    var text = $(this).sibiling(".description").val();
+    var time = $(this).parent().attr("id");
 
-  localStorage.setItem(time, text);
-});
-
-function trackHours() {
-  var currentTime = moment().hour();
-
-  // loop
-
-  $(".time-block").each(function () {
-    var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+    localStorage.setItem(time, text);
   });
-}
+
+  function trackHours() {
+    var currentTime = moment().hour();
+
+    // loop
+
+    $(".time-block").each(function () {
+      var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+      // time checker for past, present or future
+
+      if (hourBlock < currentTime) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      } else if (hourBlock === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  trackHours();
+});
