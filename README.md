d0ce0b86ab8ea6f28670f9d31578080bc984d800
Shannon McCartney <smccar117@cable.comcast.com>
2021-05-20 12:55:10 -0400

    Added images  and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# xitrumble
+# Third-Party APIs Challenge: Work Day Scheduler
+
+For this weeks challenge we were given an assignment to create a simple calendar application that allows a user to save events for each hour of the day using HTML, CSS and jQuery as well as the Moments.js library to work with date and time.
+
+## Screenshot Description
+
+Screenshots below show my Work Day Scheduler uploaded in Git Pages.
+
+### Screenshot 1
+
+1. Displays the Work Day Scheduler when the user first opens the planner.
+2. Displays the current day at the top of the calender with the use of Moments.js
+3. Displays color coded time blocks for standard business hours to indicate whether it is in the past, present or future.
+
+![Screenshot1](assets/images/Screenshot1.png)
+
+### Screenshot 2
+
+1. Displays the ablility for the user to click into a time block and add input for whatever task they might have.
+2. Displays a save button which allows the user to save text for that specific event into local storage so when the page is refreshed the saved events persist.
+
+![Screenshot1](assets/images/Screenshot2.png)
+
+## URL's
+
+- [URL of the GitHub repository](https://github.com/sdoyle0908/xitrumble)
+- [URL of deployed application](https://sdoyle0908.github.io/xitrumble/)
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   // Display current day & time
-
   var todayDate = moment().format("dddd, MMM Do YYYY");
   $("#currentDay").html(todayDate);
 
@@ -15,7 +14,7 @@ $(document).ready(function () {
   function trackHours() {
     var currentTime = moment().hour();
 
-    // loop
+    // loop through time blocks
 
     $(".time-block").each(function () {
       var thisHour = parseInt($(this).attr("id").split("hr")[1]);
@@ -37,15 +36,15 @@ $(document).ready(function () {
   }
 
   // get item from local storage
-  $("#hour9 .description").val(localStorage.getItem("hour9"));
-  $("#hour10 .description").val(localStorage.getItem("hour10"));
-  $("#hour11 .description").val(localStorage.getItem("hour11"));
-  $("#hour12 .description").val(localStorage.getItem("hour12"));
-  $("#hour13 .description").val(localStorage.getItem("hour13"));
-  $("#hour14 .description").val(localStorage.getItem("hour14"));
-  $("#hour15 .description").val(localStorage.getItem("hour15"));
-  $("#hour16 .description").val(localStorage.getItem("hour16"));
-  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hr9 .description").val(localStorage.getItem("hr9"));
+  $("#hr10 .description").val(localStorage.getItem("hr10"));
+  $("#hr11 .description").val(localStorage.getItem("hr11"));
+  $("#hr12 .description").val(localStorage.getItem("hr12"));
+  $("#hr13 .description").val(localStorage.getItem("hr13"));
+  $("#hr14 .description").val(localStorage.getItem("hr14"));
+  $("#hr15 .description").val(localStorage.getItem("hr15"));
+  $("#hr16 .description").val(localStorage.getItem("hr16"));
+  $("#hr17 .description").val(localStorage.getItem("hr17"));
 
   trackHours();
 });
